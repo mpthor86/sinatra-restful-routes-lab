dd8007c120295170c4b1785606dd2120e89313e7
michael <mpthornton86@gmail.com>
2020-12-06 13:02:18 -0700

    commit tree ad7ea3c08d33504b9c2829e97188aefd0b683a23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipe = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do 
+    #binding.pry
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+  #binding.pry
+    @recipe = Recipe.find(params[:id])
+    hash = params[:recipe]
+    @recipe.name = hash[:name]
+    @recipe.ingredients = hash[:ingredients]
+    @recipe.cook_time = hash[:cook_time]
+    @recipe.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
 end
---
+++ db/migrate/20201206174102_create_recipe_table.rb
@@ -0,0 +1,9 @@
+class CreateRecipeTable < ActiveRecord::Migration
+  def change
+    create_table :recipes do |r|
+      r.string :name
+      r.string :ingredients
+      r.string :cook_time
+    end
+  end
+end
